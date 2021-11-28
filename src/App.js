097a8098eb377f5ea93658fbbfa121dd3d07aaf7
nikki-shahh <84827772+nikki-shahh@users.noreply.github.com>
2021-11-28 12:00:29 -0800

    1.1

--- src/App.js
+++ src/App.js
@@ -8,15 +8,14 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+          Welcome to Where2Bee !        </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://nikki-shahh.github.io/where2bee"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Cooming Soon !
         </a>
       </header>
     </div>
