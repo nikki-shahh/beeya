650a71f5328985c2363e96df56c8ff54f39a2a03
nikki-shahh <84827772+nikki-shahh@users.noreply.github.com>
2021-12-05 18:51:37 -0800

    cleans the app and installs enzyme

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to Where2Bee !        </p>
-        <a
-          className="App-link"
-          href="https://nikki-shahh.github.io/where2bee"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Cooming Soon !
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
 
+      </div>
+    );
+  }
+}
 export default App;
