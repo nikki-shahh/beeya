2d45466b54d5b5c2dbdc7ae93cf6d6d1b183e1f2
nikki-shahh <84827772+nikki-shahh@users.noreply.github.com>
2022-01-11 19:45:27 -0800

    updates styling

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -6,7 +6,7 @@ class CitySearch extends Component {
     state = {
         query: '',
         suggestions: [],
-        showSuggestions: undefined
+        showSuggestions: false
     }
 
     handleInputChanged = (event) => {
--- src/EventList.js
+++ src/EventList.js
@@ -1,16 +1,21 @@
 import React from "react";
 import Event from "./Event";
+import { Row, Col } from "react-bootstrap";
 
 const EventList = ({ events }) => {
     if (!events) return null;
     return (
-        <ul className="EventList">
-            {events.map((event) => (
-                <li key={event.id}>
-                    <Event event={event} />
-                </li>
-            ))}
-        </ul>
+        <Row>
+            <Col md={10} sm={12} className="event-list-wrapper">
+                <ul className="EventList">
+                    {events.map((event) => (
+                        <li key={event.id}>
+                            <Event event={event} />
+                        </li>
+                    ))}
+                </ul>
+            </Col>
+        </Row>
     );
 };
 
